3b532536c12f6eacac492a3a3fbb18243e5307f4
coljlee <cojlee@ucdavis.edu>
2016-06-01 18:57:23 -0700

    Changed back button to start BarMain instead.

--- app/src/main/java/com/lids/barscanner/ZxingCapture.java
+++ app/src/main/java/com/lids/barscanner/ZxingCapture.java
@@ -1,9 +1,14 @@
 package com.lids.barscanner;
+import android.content.Intent;
+
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 // Empty CaptureActivity for BarMain
 public class ZxingCapture extends CaptureActivity {
     //Disable back button during scanning (causes crash)
     @Override
-    public void onBackPressed(){}
+    public void onBackPressed(){
+        Intent intent = new Intent(ZxingCapture.this, BarMain.class);
+        startActivity(intent);
+    }
 }
